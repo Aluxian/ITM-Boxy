53f733b6902e62bb941737b0fb37b826a9d4f3ed
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-05-13 11:06:28 +0300

    Initial Commit

---
+++ Boxy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Boxy
+//
+//  Created by Alexandru Rosianu on 13/05/15.
+//  Copyright (c) 2015 Aluxian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Boxy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Boxy
+//
+//  Created by Alexandru Rosianu on 13/05/15.
+//  Copyright (c) 2015 Aluxian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
